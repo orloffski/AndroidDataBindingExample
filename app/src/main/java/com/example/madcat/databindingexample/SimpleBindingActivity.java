0ea1cc096140dadb100fdb0dda86c272080b66ca
MadCat <orloffski@gmail.com>
2018-07-03 17:44:23 +0300

    Rev 0.7 get views elements from binding

--- app/src/main/java/com/example/madcat/databindingexample/SimpleBindingActivity.java
+++ app/src/main/java/com/example/madcat/databindingexample/SimpleBindingActivity.java
@@ -3,6 +3,8 @@ package com.example.madcat.databindingexample;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.madcat.databindingexample.data.Car;
 import com.example.madcat.databindingexample.data.Employee;
@@ -10,18 +12,48 @@ import com.example.madcat.databindingexample.databinding.ActivitySimpleBinding;
 
 public class SimpleBindingActivity extends AppCompatActivity {
 
+    Employee employee;
+    Car car;
+    ActivitySimpleBinding binding;
+
+    TextView carMark;
+    TextView carModel;
+    TextView carYear;
+    LinearLayout rootView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        Employee employee = new Employee("John", "Smith", 44);
-        Employee employee = null;
+        loadData();
+        bindData();
+        getViews();
 
-        Car car = new Car("Toyota", "RAV4", 2015);
-//        Car car = new Car("Toyota", "RAV4", 0);
+        updateViewsTextSize(carModel, 25f);
+    }
 
-        ActivitySimpleBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_simple);
+    public void loadData(){
+//        employee = new Employee("John", "Smith", 44);
+
+        car = new Car("Toyota", "RAV4", 2015);
+//        car = new Car("Toyota", "RAV4", 0);
+    }
+
+    public void bindData(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_simple);
         binding.setCar(car);
         binding.setEmployee(employee);
+    }
+
+    public void getViews(){
+        carMark = binding.carMark;
+        carModel = binding.carModel;
+        carYear = binding.carYear;
+
+        rootView = (LinearLayout) binding.getRoot();
+    }
+
+    public void updateViewsTextSize(TextView view, float size){
+        view.setTextSize(size);
     }
 }
